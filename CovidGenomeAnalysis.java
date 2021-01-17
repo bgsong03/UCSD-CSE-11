9f2a5dcf6b1ea4ff4966599b2e19371b3951b7f7
bgsong03 <bgsong03@gmail.com>
2021-01-17 12:32:41 +0800

    modified:   CovidGenomeAnalysis.java 	modified:   CovidMutation.java

--- CovidGenomeAnalysis.java
+++ CovidGenomeAnalysis.java
@@ -1,26 +1,44 @@
+/**
+ * Name: Bryce Ong
+ * ID: A16287711
+ * Email: bong@ucsd.edu
+ * Sources used: None
+ * 
+ * This file is used for PA#2.
+ * It contains the class CovidGenomeAnalysis
+ * which is used for Part 1 of the PA.
+ */
 import java.util.Scanner;
 
+/**
+ * This class asks for a genome sequence.
+ * It will then output a new sequence, that is the 
+ * opposite of the inputted sequence, and the number of Ts
+ * in the new sequence.
+ */
 public class CovidGenomeAnalysis {
   public static void main(String[] args){
     String new_seq = "";
     int counter = 0;
 
+    // Ask user to input a sequence
     Scanner scan = new Scanner(System.in);
     System.out.println("");
-    String seq = scan.nextLine();
+    String sequence = scan.nextLine();
 
-    for (int i = 0; i < seq.length(); i++){
-      if (seq.charAt(i) == 'A'){
+    // Loops through the entire sequence and creates a new opposite sequence
+    for (int i = 0; i < sequence.length(); i++){
+      if (sequence.charAt(i) == 'A'){
         new_seq += 'T';
         counter += 1;
       }
-      else if (seq.charAt(i) == 'T'){
+      else if (sequence.charAt(i) == 'T'){
         new_seq += 'A';
       }
-      else if (seq.charAt(i) == 'G'){
+      else if (sequence.charAt(i) == 'G'){
         new_seq += 'C';
       }
-      else if (seq.charAt(i) == 'C'){
+      else if (sequence.charAt(i) == 'C'){
         new_seq += 'G';
       }
     }
--- CovidMutation.java
+++ CovidMutation.java
@@ -1,26 +1,50 @@
+/**
+ * Name: Bryce Ong
+ * ID: A16287711
+ * Email: bong@ucsd.edu
+ * Sources used: None
+ * 
+ * This file is used for PA#2.
+ * It contains the class CovidMutation
+ * which is used for Part 2 of the PA
+ */
 import java.util.Scanner;
 
+/**
+ * This class asks for a genome sequence and an integer k. 
+ * It then reverses k-sized chunks of the sequence.
+ */
 public class CovidMutation {
   public static void main(String[] args){
     String reverse = "";
+    int k = 0;
 
+    // Ask user to input a sequence and an integer k
     Scanner scan = new Scanner(System.in);
     System.out.println("");
     String original = scan.nextLine();
     System.out.println("");
-    int k = scan.nextInt();
-
-    for (int i = 0; i < original.length(); i += k){
-      for (int j = i + k - 1; j >= i; j--){
-        if (j > original.length() - 1){
-          for (int m = original.length() -1; m >= i; m--){
-            reverse += original.charAt(m);
+    k = scan.nextInt();
+    
+    // Only able to continue if k is greater or equal to 1
+    if (k >= 1){
+      // Reverses the sequence in k-sized chunks
+      for (int i = 0; i < original.length(); i += k){
+        for (int j = i + k - 1; j >= i; j--){
+          if (j > original.length() - 1){
+            for (int m = original.length() -1; m >= i; m--){
+              reverse += original.charAt(m);
+            }
+            break;
           }
-          break;
+          reverse += original.charAt(j);
         }
-        reverse += original.charAt(j);
       }
+      System.out.println(reverse);
     }
-    System.out.println(reverse);
+    // Print original sequence if k is negative or 0
+    else{
+      System.out.println(original);
+    }
   }
 }
