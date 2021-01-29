511409ee355a42b4814ce36c1e75031643b6dc03
bgsong03 <bgsong03@gmail.com>
2021-01-29 14:11:41 +0800

    modified:   Cipher.java 	modified:   CipherEC.java

--- Cipher.java
+++ Cipher.java
@@ -1,7 +1,28 @@
+/**
+ * Name: Bryce Ong
+ * ID: A16287711
+ * Email: bong@ucsd.edu
+ * Sources used: None
+ * 
+ * This file is used for PA#3 Parts 1-3.
+ * It is used to encode and decode ciphers
+ * using the Vigenère Cipher algorithm
+ */
+
+/**
+ * This class contains 6 methods that, when used together,
+ * allow for the encoding and decoding of ciphers.
+ */
 public class Cipher {
 
+  /**
+   * This method checks if a character is lowercase.
+   *
+   * @return Boolean (True or False)
+   */
   public static boolean isLowerCase(char letter){
     int asciival = letter;
+    //Checks if character is within the ascii values for lowercase characters
     if (asciival >= 97 && asciival <= 122){
       return true;
     }
@@ -10,15 +31,22 @@ public class Cipher {
     }
   }
 
-
+  /**
+   * This method checks if a string is lowercase.
+   *
+   * @return Boolean (True or False)
+   */
   public static boolean isLowerCase(String str){
+    //If the string is empty it is still valid
     if (str == ""){
       return true;
     }
+    //If the string is null it is invalid
     else if (str == null){
       return false;
     }
     else{
+      //Loops through string to check if all characters are lowercase 
       for (int i = 0; i < str.length(); i++){
         char c = str.charAt(i);
         if (!isLowerCase(c)){
@@ -29,21 +57,35 @@ public class Cipher {
     return true;
   }
 
-
+  /**
+   * This method encodes a plain text using a key,
+   * both of which are characters.
+   *
+   * @return Encoded character plaintext
+   */
   public static char caesarShiftEncode(char plaintext, char key){
+    //Checks if plaintext and key are valid
     if (!isLowerCase(plaintext) || !isLowerCase(key)){
       return plaintext;
     }
     else{
       int asciiplain = plaintext;
       int asciikey = key;
-      int encode = ((asciiplain - 97) + (asciikey - 97)) % 26 + 97;
+      int encode = 
+      //Formula to encode using Caesar shift
+      ((asciiplain - 97) + (asciikey - 97)) % 26 + 97; 
       return (char) encode;
     }
   }
 
-
+  /**
+   * This method decodes a cipher text using a key,
+   * both of which are characters.
+   *
+   * @return Decoded character ciphertext
+   */
   public static char caesarShiftDecode(char ciphertext, char key){
+    //Checks if ciphertext and key are valid
     if (!isLowerCase(ciphertext) || !isLowerCase(key)){
       return ciphertext;
     }
@@ -51,41 +93,79 @@ public class Cipher {
       int asciicipher = ciphertext;
       int asciikey = key;
       if (asciicipher > asciikey){
-        int decode = Math.abs((asciicipher - 97) - (asciikey - 97)) % 26 + 97;
+        int decode = 
+        //Formula to decode when ascii value of ciphertext is greater than key
+        Math.abs((asciicipher - 97) - (asciikey - 97)) % 26 + 97;
         return (char) decode;
       }
       else{
-        int decode = (26 - Math.abs((asciicipher - 97) - (asciikey - 97))) % 26 + 97;
+        int decode = 
+        //Formula to decode when ascii value of key is greater than ciphertext
+        (26 - Math.abs((asciicipher - 97) - (asciikey - 97))) % 26 + 97;
         return (char) decode;
       }
     }
   }
 
-
+  /**
+   * This method encodes a plain text using a key,
+   * both of which are strings.
+   *
+   * @return Encoded string plaintext
+   */
   public static String vigenereEncode(String plaintext, String key){
-    int counter = 0;
-    String vigEncode = "";
-    for (int i = 0; i < plaintext.length(); i++){
-      if (counter > key.length() - 1){
-        counter = 0;
-      }
-      vigEncode += caesarShiftEncode(plaintext.charAt(i), key.charAt(counter));
-      counter++;
+    //For Part 4 of PA3
+    //Checks if key and plaintext are valid
+    if (key == "" || !isLowerCase(plaintext) || !isLowerCase(key)){
+      return null;
     }
-    return vigEncode;
+    else{
+      int counter = 0;
+      //Create an empty string to add the encoded characters to
+      String vigEncode = "";
+      //Loop through plaintext
+      for (int i = 0; i < plaintext.length(); i++){
+        //Counter to check if the key needs to wrap around
+        if (counter > key.length() - 1){
+          counter = 0;
+        }
+        //Add encoded character to vigEncode
+        vigEncode += 
+        caesarShiftEncode(plaintext.charAt(i), key.charAt(counter));
+        counter++;
+    }
+      return vigEncode;
+    }
   }
 
-
+  /**
+   * This method decodes a cipher text using a key,
+   * both of which are strings.
+   *
+   * @return Decoded string ciphertext
+   */
   public static String vigenereDecode(String ciphertext, String key){
-    int counter = 0;
-    String vigDecode = "";
-    for (int i = 0; i < ciphertext.length(); i++){
-      if (counter > key.length() - 1){
-        counter = 0;
-      }
-      vigDecode += caesarShiftDecode(ciphertext.charAt(i), key.charAt(counter));
-      counter++;
+    //For Part 4 of PA3
+    //Checks if key and ciphertext are valid
+    if (key == "" || !isLowerCase(ciphertext) || !isLowerCase(key)){
+      return null;
     }
-    return vigDecode;
+    else{
+      int counter = 0;
+      //Create an empty string to add the decoded characters to
+      String vigDecode = "";
+      //Loop through ciphertext
+      for (int i = 0; i < ciphertext.length(); i++){
+        //Counter to check if the key needs to wrap around
+        if (counter > key.length() - 1){
+          counter = 0;
+        }
+        //Add decoded character to vigDecode
+        vigDecode += 
+        caesarShiftDecode(ciphertext.charAt(i), key.charAt(counter));
+        counter++;
+      }
+      return vigDecode;
+    }
   }
 }
--- CipherEC.java
+++ CipherEC.java
@@ -1,3 +1,93 @@
+/**
+ * Name: Bryce Ong
+ * ID: A16287711
+ * Email: bong@ucsd.edu
+ * Sources used:  Zybooks chapter 5.4 (findGCD & findLCM)
+ * 
+ * This file is used for PA#3 Part 4.
+ * It is used to encode and decode ciphers
+ * using the Vernam Cipher algorithm.
+ */
+
+/**
+ * This class contains 5 methods that, when used together,
+ * allow for the encoding and decoding of ciphers.
+ */
 public class CipherEC {
-  
+
+  /**
+   * This method finds the greatest common divisor of two values.
+   *
+   * @return GCD of aVal and bVal
+   */
+  public static int findGCD(int aVal, int bVal) {
+    int numA;
+    int numB;
+    numA = aVal;
+    numB = bVal;
+
+    while (numA != numB) { // Euclid's algorithm
+      if (numB > numA) {
+        numB = numB - numA;
+      } 
+      else {
+        numA = numA - numB;
+      }
+    }
+    return numA;
+  }
+
+  /**
+   * This method finds the lowest common multiple of two values.
+   *
+   * @return LCM of aVal and bVal
+   */
+  public static int findLCM(int aVal, int bVal){
+    int lcmVal;
+    lcmVal = Math.abs(aVal * bVal) / findGCD(aVal, bVal);
+    return lcmVal;
+  }
+
+  /**
+   * This method creates a long key using two short keys.
+   *
+   * @return Long key for Vernam Cipher
+   */
+  public static String computeVernamLongKey(String key1, String key2){
+    //Find LCM for the two keys
+    int lcmVal = findLCM(key1.length(), key2.length());
+    //Repeats key1 until its length reaches the LCM
+    while(key1.length() < lcmVal){
+      key1 += key1;
+    }
+    //Repeats key2 until its length reaches the LCM
+    while(key2.length() < lcmVal){
+      key2 += key2;
+    }
+    //Creates long key using the two keys
+    String longKey = Cipher.vigenereEncode(key1, key2);
+    return longKey;
+  }
+
+  /**
+   * This method encodes plaintext using the long key
+   *
+   * @return Encoded plaintext
+   */
+  public static String vernamEncode(String plaintext, String key1, String key2){
+    String longKey = computeVernamLongKey(key1, key2);
+    String encode = Cipher.vigenereEncode(plaintext, longKey);
+    return encode;
+  }
+
+  /**
+   * This method decodes ciphertext using the long key
+   *
+   * @return Decoded ciphertext
+   */
+  public static String vernamDecode(String ciphertext, String key1, String key2){
+    String longKey = computeVernamLongKey(key1, key2);
+    String decode = Cipher.vigenereDecode(ciphertext, longKey);
+    return decode;
+  }
 }
