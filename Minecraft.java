16eede907dfb7f4d5216762bfad9177310665ac8
bgsong03 <bgsong03@gmail.com>
2021-02-12 10:04:05 +0800

    Finished rotateFloorPlan method

--- Minecraft.java
+++ Minecraft.java
@@ -1,18 +1,40 @@
+import java.util.*;
+
 public class Minecraft {
-  private static final int N = 4;
   public static int[][] rotateFloorPlan(int[][] originalFloorPlan){
     if (originalFloorPlan != null){
-      for (int i = 0; i < N / 2; i++){
-        for (int j = i; j < N - i - 1; j++){
-            int temp = originalFloorPlan[i][j];
-            originalFloorPlan[i][j] = originalFloorPlan[N - 1 - j][i];
-            originalFloorPlan[N - 1 - j][i] = originalFloorPlan[N - 1 - i][N - 1 - j];
-            originalFloorPlan[N - 1 - i][N - 1 - j] = originalFloorPlan[j][N - 1 - i];
-            originalFloorPlan[j][N - 1 - i] = temp;
+      int column = originalFloorPlan.length;
+      int row = originalFloorPlan[0].length;
+      int [][] newFloorPlan = new int [row][column];
+      for (int i = 0; i < column; i++){
+        for (int j = 0; j < row; j++){
+          newFloorPlan[j][column - 1 - i] = originalFloorPlan[i][j];
         }
       }
-      return originalFloorPlan;
+      return newFloorPlan;
     }
     return null;
+  }
+
+  public static ArrayList<String> getMobsToTest(String[][] groups,
+  String infected){
+    if (groups != null && infected != null){
+      ArrayList<String> infectedMobs = new ArrayList<String>();
+      return infectedMobs;
+    }
+    return null;
+  }
+  public static void main(String args[]){
+    String[][] groups = {
+      {"Zombie", "Cow", "Creeper", "Guardian", null, "Slime"},
+      {"Sheep", "Iron Golem", "Cow"},
+      {"Sheep"},
+      {"Cow", "Puf erfish", "Squid", "Sheep"}
+    };
+    ArrayList<String> test = new ArrayList<String>();
+    test = getMobsToTest(groups, "Sheep");
+    for (int i = 0; i < test.size();i++){ 		      
+      System.out.println(test.get(i)); 		
+    }   
   }
 }
